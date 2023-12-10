16e24beda410f51bb42c0a7da4acbaa54d57f2c9
Jeremy Bl <jeremybl2004@gmail.com>
2023-12-10 18:50:04 -0500

    Made Changes

--- SensorReadings.py
+++ SensorReadings.py
@@ -1,91 +1,106 @@
 #!/usr/bin/env python
+import os
+import RPi.GPIO as GPIO
 import ADC0832
 import time
-import threading
-import RPi.GPIO as GPIO
 import math
 
-# Constants for the thermistor characteristics
-R0 = 10000  # Resistance at a known temperature (in ohms)
-T0 = 25     # Known temperature in Celsius (adjust as needed)
-B = 3950    # Beta coefficient of the thermistor (adjust as needed)
+# Motor Pins
+FAN_PIN_A = 6
+FAN_PIN_B = 13
 
-# GPIO pins for the LEDs
-LED1_PIN = 23
-LED2_PIN = 24
+# Global Variables
+fan_cooldown = False
 
-def init():
-    ADC0832.setup()
-    GPIO.setwarnings(False)
+def setup():
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(LED1_PIN, GPIO.OUT)  # Set up LED1 pin as an output
-    GPIO.setup(LED2_PIN, GPIO.OUT)  # Set up LED2 pin as an output
-    GPIO.output(LED1_PIN, GPIO.LOW)  # Turn off LED1 initially
-    GPIO.output(LED2_PIN, GPIO.LOW)  # Turn off LED2 initially
+    GPIO.setup(FAN_PIN_A, GPIO.OUT)
+    GPIO.setup(FAN_PIN_B, GPIO.OUT)
+    GPIO.output(FAN_PIN_A, GPIO.HIGH)
+    GPIO.output(FAN_PIN_B, GPIO.HIGH)
+    stop_fan()
+    ADC0832.setup()
 
-def map_value(value, from_min, from_max, to_min, to_max):
-    # Map 'value' from the range [from_min, from_max] to [to_min, to_max]
-    return (value - from_min) * (to_max - to_min) / (from_max - from_min) + to_min
+# Stop the fan
+def stop_fan():
+    GPIO.output(FAN_PIN_A, GPIO.HIGH)
+    GPIO.output(FAN_PIN_B, GPIO.HIGH)
 
-def temperature_from_resistance(Rt):
-    try:
-        # Calculate temperature in Celsius using the Steinhart-Hart equation
-        inv_T = 1.0 / (T0 + 273.15) + (1.0 / B) * math.log(Rt / R0)
-        temperature_C = 1.0 / inv_T - 273.15
-        return temperature_C
-    except ValueError:
-        # Handle the case where math.log() receives an invalid argument
-        return None
-
-def update_temperature():
-    while True:
-        # Read photoresistor and thermistor values from ADC channels
-        res_light = ADC0832.getADC(0)  # Photoresistor connected to channel 0
-        res_temp = ADC0832.getADC(1)   # Thermistor connected to channel 1
-
-        # Check the light level and update the labels
-        if res_light < 128:
-            # Turn on both LEDs when it's dark
-            GPIO.output(LED1_PIN, GPIO.HIGH)
-            GPIO.output(LED2_PIN, GPIO.HIGH)
+# Control fan operation
+def control_fan(status=0, direction=1):
+    if status == 0:  # stop
+        stop_fan()
+        print("Fan is Off")
+    else:
+        if direction == 1:
+            print("Fan is On")
+            GPIO.output(FAN_PIN_A, GPIO.HIGH)
+            GPIO.output(FAN_PIN_B, GPIO.LOW)
         else:
-            # Turn off both LEDs when it's light
-            GPIO.output(LED1_PIN, GPIO.LOW)
-            GPIO.output(LED2_PIN, GPIO.LOW)
+            GPIO.output(FAN_PIN_A, GPIO.LOW)
+            GPIO.output(FAN_PIN_B, GPIO.HIGH)
 
-        # Calculate temperature in Celsius
-        Vr_temp = 3.3 * float(res_temp) / 255
-        Rt_temp = 10000 * Vr_temp / (3.3 - Vr_temp)
-        temperature_C = temperature_from_resistance(Rt_temp)
+# Run the fan with cooldown
+def run_fan_with_cooldown():
+    global fan_cooldown
 
-        if temperature_C is not None:
-            temperature_F = (temperature_C * 9/5) + 32  # Convert to Fahrenheit
+    if not fan_cooldown:
+        fan_cooldown = True
+        control_fan(1)
+        time.sleep(30)
+        control_fan(0)
+        fan_cooldown = False
 
-            print(f'Light Status: {"Dark" if res_light < 128 else "Light"}')
-            print(f'Temperature (Celsius): {temperature_C:.2f}°C')
+# Read temperature from sensor
+def read_temperature_sensor():
+    T25 = 25 + 273.15  # Convert to Kelvin
+    R25 = 10000  # Resistance for degrees in Celsius
+    B = 3455
 
-        time.sleep(0.2)
+    res = ADC0832.getADC(0)
+    Vr = 3.3 * float(res) / 255
+
+    try:
+        # Handle the case where Rt becomes zero
+        Rt = (3.3 * 10000) / Vr - 10000
+        if Rt == 0:
+            Rt = 0.1
+    except ZeroDivisionError:
+        Rt = 0.1
+
+    ln = math.log(Rt / R25)
+    Tk = 1 / ((ln / B) + (1 / T25))
+    Tc = Tk - 273.15  # Convert to Celsius
+
+    Tc = round(Tc, 2)
+    return Tc
+
+# Manual override to control the fan
+def manual_fan_control(status=False):
+    if status:
+        control_fan(1)
+    elif not fan_cooldown:
+        control_fan(0)
+
+# Main loop
+def main_loop():
+    while True:
+        temperature_C = read_temperature_sensor()
+        print(f'Temperature (Celsius): {temperature_C:.2f}°C')
+
+        # Adjust this condition for fan control
+        if temperature_C > 30:
+            run_fan_with_cooldown()
+
+        time.sleep(2)
 
 def cleanup():
-    # Turn off both LEDs
-    GPIO.output(LED1_PIN, GPIO.LOW)
-    GPIO.output(LED2_PIN, GPIO.LOW)
-    # Clean up GPIO
     GPIO.cleanup()
 
-def main():
-    init()
-
-    # Start the temperature update thread
-    update_thread = threading.Thread(target=update_temperature)
-    update_thread.daemon = True
-    update_thread.start()
-
+if __name__ == '__main__':
+    setup()
     try:
-        while True:
-            pass
+        main_loop()
     except KeyboardInterrupt:
         cleanup()
-
-if __name__ == '__main__':
-    main()
+        ADC0832.destroy()
