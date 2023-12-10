3810fc560dd6e3ca9536d9ca192fd4002e686375
Jeremy Bl <jeremybl2004@gmail.com>
2023-12-10 18:24:53 -0500

    Made Changes

--- SensorReadings.py
+++ SensorReadings.py
@@ -1,107 +1,105 @@
 #!/usr/bin/env python
-import time
 import ADC0832
+import time
+import threading
 import RPi.GPIO as GPIO
-import math
 
 # Constants for the thermistor characteristics
 R0 = 10000  # Resistance at a known temperature (in ohms)
 T0 = 25     # Known temperature in Celsius (adjust as needed)
 B = 3950    # Beta coefficient of the thermistor (adjust as needed)
 
-# GPIO pin for photoresistor
-PHOTORESISTOR_PIN = 0
+# GPIO pins for the LEDs
+LED1_PIN = 23
+LED2_PIN = 24
 
-# GPIO pin for thermistor
-THERMISTOR_PIN = 1
+# Define the potentiometer reading range
+POT_MIN = 0    # Minimum ADC value for the potentiometer
+POT_MAX = 255  # Maximum ADC value for the potentiometer
 
-# GPIO pin for LED
-LED_PIN = 21
+def init():
+    ADC0832.setup()
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(LED1_PIN, GPIO.OUT)  # Set up LED1 pin as an output
+    GPIO.setup(LED2_PIN, GPIO.OUT)  # Set up LED2 pin as an output
+    GPIO.output(LED1_PIN, GPIO.LOW)  # Turn off LED1 initially
+    GPIO.output(LED2_PIN, GPIO.LOW)  # Turn off LED2 initially
 
-# Thresholds for light and temperature
-LIGHT_THRESHOLD = 128
-TEMPERATURE_THRESHOLD = 30  # Adjust as needed
+def map_value(value, from_min, from_max, to_min, to_max):
+    # Map 'value' from the range [from_min, from_max] to [to_min, to_max]
+    return (value - from_min) * (to_max - to_min) / (from_max - from_min) + to_min
 
 def temperature_from_resistance(Rt):
     try:
         # Calculate temperature in Celsius using the Steinhart-Hart equation
-        inv_T = 1.0 / (T0 + 273.15) + (1.0 / B) * math.log(Rt / R0)
+        inv_T = 1.0 / (T0 + 273.15) + (1.0 / B) * (Rt / R0)
         temperature_C = 1.0 / inv_T - 273.15
         return temperature_C
     except ValueError:
         # Handle the case where math.log() receives an invalid argument
         return None
 
-def read_photoresistor():
-    try:
-        # Read photoresistor value from ADC channel 0
-        photoresistor_value = ADC0832.getADC(PHOTORESISTOR_PIN)
-        return photoresistor_value
-    except IOError as e:
-        # Handle ADC read error
-        print(f"Error reading photoresistor: {e}")
-        return None
-
-def read_thermistor():
-    try:
-        # Read thermistor value from ADC channel 1
-        thermistor_value = ADC0832.getADC(THERMISTOR_PIN)
-        return thermistor_value
-    except IOError as e:
-        # Handle ADC read error
-        print(f"Error reading thermistor: {e}")
-        return None
-
-def init():
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(LED_PIN, GPIO.OUT)  # Set up LED pin as an output
-    GPIO.output(LED_PIN, GPIO.LOW)  # Turn off the LED initially
-
-def update_light_and_temperature():
+def update_temperature():
     while True:
-        try:
-            # Read photoresistor value
-            photoresistor_value = read_photoresistor()
-            if photoresistor_value is not None:
-                light_status = "Light" if photoresistor_value >= LIGHT_THRESHOLD else "Dark"
-                print(f"Light Status: {light_status}")
+        # Read temperature and potentiometer values from ADC channels
+        res_temp = ADC0832.getADC(1)  # Temperature sensor connected to channel 0
+        res_pot = ADC0832.getADC(0)   # Potentiometer connected to channel 1
 
-                # Control LED based on light status
-                GPIO.output(LED_PIN, GPIO.HIGH if light_status == "Dark" else GPIO.LOW)
+        Vr_temp = 3.3 * float(res_temp) / 255
+        Vr_pot = 3.3 * float(res_pot) / 255
 
-            # Read thermistor value
-            thermistor_value = read_thermistor()
-            if thermistor_value is not None:
-                # Convert thermistor value to temperature in degrees Celsius
-                Rt_temp = 3.3 * float(thermistor_value) / 255
-                temperature_C = temperature_from_resistance(Rt_temp)
-                print(f"Temperature: {temperature_C:.2f} degrees Celsius")
+        Rt_temp = 10000 * Vr_temp / (3.3 - Vr_temp)
 
-                # Check temperature and perform actions
-                if temperature_C is not None and temperature_C > TEMPERATURE_THRESHOLD:
-                    # Add logic to control fan, water pump, etc. based on temperature
-                    print("Temperature exceeds threshold. Perform actions here.")
+        # Calculate temperature in Celsius
+        temperature_C = temperature_from_resistance(Rt_temp)
 
-            time.sleep(1)
-        except Exception as e:
-            print(f"Unexpected error: {e}")
-            GPIO.cleanup()  # Clean up GPIO on error
-            break
+        if temperature_C is not None:
+            temperature_F = (temperature_C * 9/5) + 32  # Convert to Fahrenheit
+
+            # Map potentiometer value to temperature threshold
+            temperature_threshold = map_value(res_pot, POT_MIN, POT_MAX, 50, -50)  # Inverted mapping
+
+            # Ensure the threshold doesn't exceed the specified range
+            temperature_threshold = max(-50, min(50, temperature_threshold))
+
+            print(f'Temperature (Celsius): {temperature_C:.2f}°C')
+            print(f'Temperature (Fahrenheit): {temperature_F:.2f}°F')
+
+            # Turn on both LEDs if it's dark
+            res_light = ADC0832.getADC(0)  # Photoresistor connected to channel 0
+
+            # Check the light level and update the labels
+            if res_light < 128:
+                # Turn on both LEDs when it's dark
+                GPIO.output(LED1_PIN, GPIO.HIGH)
+                GPIO.output(LED2_PIN, GPIO.HIGH)
+            else:
+                # Turn off both LEDs when it's light
+                GPIO.output(LED1_PIN, GPIO.LOW)
+                GPIO.output(LED2_PIN, GPIO.LOW)
+
+        time.sleep(0.2)
 
 def cleanup():
+    # Turn off both LEDs
+    GPIO.output(LED1_PIN, GPIO.LOW)
+    GPIO.output(LED2_PIN, GPIO.LOW)
     # Clean up GPIO
     GPIO.cleanup()
 
 def main():
     init()
 
+    # Start the temperature update thread
+    update_thread = threading.Thread(target=update_temperature)
+    update_thread.daemon = True
+    update_thread.start()
+
     try:
-        ADC0832.setup()
-        update_light_and_temperature()
+        while True:
+            pass
     except KeyboardInterrupt:
-        pass
-    finally:
         cleanup()
 
 if __name__ == '__main__':
