cbf45eec66c814ea761670c67cad4ac4f501ec5e
Jeremy Bl <jeremybl2004@gmail.com>
2023-12-10 17:44:09 -0500

    ADC Script

--- ADC0832.py
+++ ADC0832.py
@@ -1,70 +1,76 @@
 #!/usr/bin/env python
-import RPi.GPIO as GPIO
-import time
+#
+# Analog Input with ADC0832 chip
+#
+# Datasheet: http://www.ti.com/lit/ds/symlink/adc0838-n.pdf
+# Part of SunFounder LCD StarterKit
+# http://www.sunfounder.com/index.php?c=show&id=21&model=LCD%20Starter%20Kit
+#
 
-ADC_CS  = 11
-ADC_CLK = 13
-ADC_DIO = 12
+import time
+import os
+import RPi.GPIO as GPIO
+
+# change these as desired - they're the pins connected from the
+# SPI port on the ADC to the Cobbler
+PIN_CLK = 18
+PIN_DO  = 27
+PIN_DI  = 22
+PIN_CS  = 17
 
 def setup():
-	GPIO.setwarnings(False)
-	GPIO.setmode(GPIO.BOARD)    #Number GPIOs by its physical location
-	GPIO.setup(ADC_CS, GPIO.OUT)
-	GPIO.setup(ADC_CLK, GPIO.OUT)
-
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    # set up the SPI interface pins
+    GPIO.setup(PIN_DI,  GPIO.OUT)
+    GPIO.setup(PIN_DO,  GPIO.IN)
+    GPIO.setup(PIN_CLK, GPIO.OUT)
+    GPIO.setup(PIN_CS,  GPIO.OUT)
+    
 def destroy():
-	GPIO.cleanup()
+	GPIO.cleanup()    
 
-def getResult():     # get ADC result
-	GPIO.setup(ADC_DIO, GPIO.OUT)
-	GPIO.output(ADC_CS, 0)
-	
-	GPIO.output(ADC_CLK, 0)
-	GPIO.output(ADC_DIO, 1);  time.sleep(0.000002)
-	GPIO.output(ADC_CLK, 1);  time.sleep(0.000002)
-	GPIO.output(ADC_CLK, 0)
+# read SPI data from ADC8032
+def getADC(channel):
+	# 1. CS LOW.
+        GPIO.output(PIN_CS, True)      # clear last transmission
+        GPIO.output(PIN_CS, False)     # bring CS low
 
-	GPIO.output(ADC_DIO, 1);  time.sleep(0.000002)
-	GPIO.output(ADC_CLK, 1);  time.sleep(0.000002)
-	GPIO.output(ADC_CLK, 0)
+	# 2. Start clock
+        GPIO.output(PIN_CLK, False)  # start clock low
 
-	GPIO.output(ADC_DIO, 0);  time.sleep(0.000002)
+	# 3. Input MUX address
+        for i in [1,1,channel]: # start bit + mux assignment
+                 if (i == 1):
+                         GPIO.output(PIN_DI, True)
+                 else:
+                         GPIO.output(PIN_DI, False)
 
-	GPIO.output(ADC_CLK, 1)
-	GPIO.output(ADC_DIO, 1);  time.sleep(0.000002)
-	GPIO.output(ADC_CLK, 0)
-	GPIO.output(ADC_DIO, 1);  time.sleep(0.000002)
-	
-	dat1 = 0
-	for i in range(0, 8):
-		GPIO.output(ADC_CLK, 1);  time.sleep(0.000002)
-		GPIO.output(ADC_CLK, 0);  time.sleep(0.000002)
-		GPIO.setup(ADC_DIO, GPIO.IN)
-		dat1 = dat1 << 1 | GPIO.input(ADC_DIO)  # or ?
-	
-	dat2 = 0
-	for i in range(0, 8):
-		dat2 = dat2 | GPIO.input(ADC_DIO) << i
-		GPIO.output(ADC_CLK, 1);  time.sleep(0.000002)
-		GPIO.output(ADC_CLK, 0);  time.sleep(0.000002)
-	
-	GPIO.output(ADC_CS, 1)
-	GPIO.setup(ADC_DIO, GPIO.OUT)
+                 GPIO.output(PIN_CLK, True)
+                 GPIO.output(PIN_CLK, False)
 
-	if dat1 == dat2:
-		return dat1
-	else:
-		return 0
+        # 4. read 8 ADC bits
+        ad = 0
+        for i in range(8):
+                GPIO.output(PIN_CLK, True)
+                GPIO.output(PIN_CLK, False)
+                ad <<= 1 # shift bit
+                if (GPIO.input(PIN_DO)):
+                        ad |= 0x1 # set first bit
+
+        # 5. reset
+        GPIO.output(PIN_CS, True)
+
+        return ad
 
 def loop():
-	while True:
-		res = getResult()
-		print('res = %d' % res)
-		time.sleep(0.4)
+    while True:
+        print("ADC[0]: {}\t ADC[1]: {}".format(getADC(0), getADC(1)))
+        time.sleep(1)
 
-if __name__ == '__main__':
-	setup()
-	try:
-		loop()
-	except KeyboardInterrupt:
-		destroy()
+if __name__ == "__main__":
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
