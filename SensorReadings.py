376c1004b761806e2b6dcd168ee83c1c37dda552
Jeremy Bl <jeremybl2004@gmail.com>
2023-12-10 19:25:35 -0500

    Final changes to script before dashboard implementations

--- SensorReadings.py
+++ SensorReadings.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
-import os
 import RPi.GPIO as GPIO
-import ADC0832
 import Adafruit_DHT
+import ADC0832
 import time
-import math
 
 # Motor Pins
 FAN_PIN_A = 6
 FAN_PIN_B = 13
-MOISTURE_SENSOR_PIN = 26  # GPIO for the DHT-11 sensor
+MOISTURE_SENSOR_PIN = 1  # GPIO for the Soil Moisture Detector
+RELAY_PIN = 4  # GPIO for the Relay Module
 
 # Global Variables
 fan_cooldown = False
@@ -18,10 +17,11 @@ def setup():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(FAN_PIN_A, GPIO.OUT)
     GPIO.setup(FAN_PIN_B, GPIO.OUT)
+    GPIO.setup(RELAY_PIN, GPIO.OUT)  # Relay Module control pin
     GPIO.output(FAN_PIN_A, GPIO.HIGH)
     GPIO.output(FAN_PIN_B, GPIO.HIGH)
+    GPIO.output(RELAY_PIN, GPIO.HIGH)  # Turn off the water pump initially
     stop_fan()
-    ADC0832.setup()
 
 def stop_fan():
     GPIO.output(FAN_PIN_A, GPIO.HIGH)
@@ -46,53 +46,48 @@ def run_fan_with_cooldown():
     if not fan_cooldown:
         fan_cooldown = True
         control_fan(1)
-        time.sleep(15)
+        time.sleep(30)
         control_fan(0)
         fan_cooldown = False
 
 def read_temperature_sensor():
-    T25 = 25 + 273.15
-    R25 = 10000
-    B = 3455
-
-    res = ADC0832.getADC(0)
-    Vr = 3.3 * float(res) / 255
-
-    try:
-        Rt = (3.3 * 10000) / Vr - 10000
-        if Rt == 0:
-            Rt = 0.1
-    except ZeroDivisionError:
-        Rt = 0.1
-
-    ln = math.log(Rt / R25)
-    Tk = 1 / ((ln / B) + (1 / T25))
-    Tc = Tk - 273.15
-
-    Tc = round(Tc, 2)
-    return Tc
-
-def read_moisture_sensor():
-    # Use the Adafruit_DHT library to read data from the DHT-11 sensor
     sensor = Adafruit_DHT.DHT11
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, MOISTURE_SENSOR_PIN)
+    _, temperature = Adafruit_DHT.read_retry(sensor, MOISTURE_SENSOR_PIN)
 
-    if humidity is not None and temperature is not None:
-        print(f'Moisture: {humidity}%')
+    if temperature is not None:
         print(f'Temperature (DHT-11): {temperature:.2f}°C')
+        return temperature
     else:
         print('Failed to read data from DHT-11 sensor')
+        return None
+
+def read_soil_moisture_sensor():
+    moisture_value = ADC0832.getResult(MOISTURE_SENSOR_PIN)
+    print(f'Soil Moisture: {moisture_value}')
+    return moisture_value
+
+def control_water_pump(status=0):
+    if status == 1:
+        GPIO.output(RELAY_PIN, GPIO.LOW)  # Turn on the water pump
+        print("Water Pump is On")
+    else:
+        GPIO.output(RELAY_PIN, GPIO.HIGH)  # Turn off the water pump
+        print("Water Pump is Off")
 
 def main_loop():
     while True:
         temperature_C = read_temperature_sensor()
-        print(f'Temperature (Celsius): {temperature_C:.2f}°C')
+        moisture_value = read_soil_moisture_sensor()
 
-        read_moisture_sensor()
-
-        # Adjust this condition for fan control
-        if temperature_C > 30:
+        # Adjust this condition for fan control based on temperature or moisture
+        if temperature_C is not None and temperature_C > 30:
             run_fan_with_cooldown()
+
+        # Adjust this condition for water pump control based on soil moisture
+        if moisture_value is not None and moisture_value < 100:  # Adjust the threshold as needed
+            control_water_pump(1)  # Turn on the water pump
+        else:
+            control_water_pump(0)  # Turn off the water pump
 
         time.sleep(2)
 
